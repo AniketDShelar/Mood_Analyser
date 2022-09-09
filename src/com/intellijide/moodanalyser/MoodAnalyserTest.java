2c837afa8a40f887908233b5a27a997240eb9b9d
AniketDShelar <theaniketshelar@gmail.com>
2022-09-09 14:18:33 +0530

    tc-1 testing mood analyser

---
+++ src/com/intellijide/moodanalyser/MoodAnalyserTest.java
@@ -0,0 +1,16 @@
+package com.intellijide.moodanalyser;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MoodAnalyserTest {
+
+    @Test
+    void analyseMood() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        String actual = moodAnalyser.analyseMood("I am in sad mood");
+        Assertions.assertEquals("sad",actual);
+    }
+}
