acfa0a12194a34a0b6f55b69ca93bc79745c02a0
AniketDShelar <theaniketshelar@gmail.com>
2022-09-09 14:30:09 +0530

    handle exception if user provide invalid mood

--- src/com/intellijide/moodanalyser/MoodAnalyser.java
+++ src/com/intellijide/moodanalyser/MoodAnalyser.java
@@ -1,16 +1,19 @@
 package com.intellijide.moodanalyser;
 
 public class MoodAnalyser {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         MoodAnalyser moodAnalyser = new MoodAnalyser();
-        moodAnalyser.analyseMood("I am in sad mood ");
+        moodAnalyser.analyseMood(null);
     }
 
         public String analyseMood(String message) {
-            if (message.contains("sad")){
-                return "sad";
-            }else {
-                return "happy";
+            try{
+                if (message.contains("sad"))
+                    return "sad";
+
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
             }
+                return "Happy";
         }
     }
--- src/com/intellijide/moodanalyser/MoodAnalyserTest.java
+++ src/com/intellijide/moodanalyser/MoodAnalyserTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class MoodAnalyserTest {
+public class MoodAnalyserTest {
 
     @Test
     void analyseMood() {
