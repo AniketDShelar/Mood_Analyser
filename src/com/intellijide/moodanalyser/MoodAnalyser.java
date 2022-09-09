506fdc3191c5c556c9abab4c2c70fa17fd15f0cb
AniketDShelar <theaniketshelar@gmail.com>
2022-09-09 14:14:11 +0530

    Create object

---
+++ src/com/intellijide/moodanalyser/MoodAnalyser.java
@@ -0,0 +1,16 @@
+package com.intellijide.moodanalyser;
+
+public class MoodAnalyser {
+    public static void main(String[] args) {
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        moodAnalyser.analyseMood("I am in sad mood ");
+    }
+
+        public String analyseMood(String message) {
+            if (message.contains("sad")){
+                return "sad";
+            }else {
+                return "happy";
+            }
+        }
+    }
