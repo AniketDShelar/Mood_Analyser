dad029ed59d7d8c36e829fa022409f24b0558193
AniketDShelar <theaniketshelar@gmail.com>
2022-09-09 14:52:22 +0530

    analyze sad mood

--- src/com/intellijide/moodanalyser/MoodAnalyser.java
+++ src/com/intellijide/moodanalyser/MoodAnalyser.java
@@ -3,7 +3,7 @@ package com.intellijide.moodanalyser;
 public class MoodAnalyser {
     public static void main(String[] args) {
         MoodAnalyser moodAnalyser = new MoodAnalyser();
-        moodAnalyser.analyseMood("I am in sad mood ");
+        moodAnalyser.analyseMood("I am in sad mood");
     }
 
         public String analyseMood(String message) {
