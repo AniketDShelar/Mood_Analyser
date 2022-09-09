3920a7de9df5a27eb79d2f22167bbc4540b0238e
AniketDShelar <theaniketshelar@gmail.com>
2022-09-09 15:30:05 +0530

    handle exceptions

--- src/com/intellijide/moodanalyser/MoodAnalyser.java
+++ src/com/intellijide/moodanalyser/MoodAnalyser.java
@@ -3,7 +3,7 @@ package com.intellijide.moodanalyser;
 public class MoodAnalyser {
     public static void main(String[] args){
         MoodAnalyser moodAnalyser = new MoodAnalyser();
-        moodAnalyser.analyseMood("I am in any mood");
+        moodAnalyser.analyseMood("I am in any mood ");
     }
 
         public String analyseMood(String message) {
